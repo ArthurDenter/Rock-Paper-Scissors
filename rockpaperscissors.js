77ea02c1fc50cea40a3ea3c3514d257c6b42566a
ArthurDenter <joerg.adomaitis@icloud.com>
2023-05-30 13:22:09 +0200

    Add class player, test method getRandomChoice
    Changes to be committed:
    	modified:   index.html
    	modified:   rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,12 +1,46 @@
-//computer simulated counterpart
-function getComputerChoice(){
+function playSingleRound(playerSelection, userSelection) {
     //code here
 };
 
-function playSingleRound(playerSelection, userSelection){
-    //code here
+function game() {
+    //create class player with properties: type, choice, wins
+    class player {
+        constructor(type) {
+            this.type = type;
+            this.choice = "";
+            this.wins = 0;
+        };
+
+        //make a random decision whether to use stone, paper, or scissors
+        getRandomChoice() {
+            //create empty string variable computerChoice
+            let computerChoice = "";
+            //get random integer with a value of 0, 1 or 2
+            let getRandomInt = Math.floor(Math.random() * 3);
+            // if return value from getRandomInt == 0 then botCounterpartChoice is "rock"
+            // if return value from getRandomInt == 1 then botCounterpartChoice is "paper"
+            // if return value from getRandomInt == 2 then botCounterpartChoice is "scissors"
+            switch (getRandomInt) {
+                case 0:
+                    computerChoice = "rock";
+                    break;
+                case 1:
+                    computerChoice = "paper";
+                    break;
+                case 2:
+                    computerChoice = "scissors";
+            };
+            return computerChoice;
+        };
+    };
+    //create an object computer of class player and set type = computer
+    let computer = new player("computer");
+    //create an object human of class player and set type = human
+    let human = new player("human");
+
+    //test getRandomChoice => console.log return value
+    computer.choice = computer.getRandomChoice();
+    console.log(computer.choice);
 };
 
-function game(){
-    //code here
-};
+game();
